5630622c51deee394e64589a9ba107c51a64927f
clone278 <clone278@gmail.com>
2015-09-20 11:38:53 +1000

    Adds Windows Service project and ps files

---
+++ VillageCinemas.WindowsService/AardvarkService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VillageCinemas.WindowsService
+{
+    public partial class AardvarkService : ServiceBase
+    {
+        public AardvarkService()
+        {
+            InitializeComponent();
+
+            this.ServiceName = "Aardvark Service";
+            this.EventLog.Log = "Application";
+
+            this.CanStop = true;
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            base.OnStart(args);
+        }
+
+        protected override void OnStop()
+        {
+            base.OnStop();
+        }
+    }
+}
